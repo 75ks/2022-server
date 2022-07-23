29653fd50415d75f0f21b000915989b5b311b996
75ks <kenta.s705@gmail.com>
2022-07-23 19:29:14 +0900

    コントローラー実行前の共通処理で認証に失敗した場合に401エラーを投げるように修正した

--- src/main/java/com/c4c/_2022server/config/HandlerInterceptorImpl.java
+++ src/main/java/com/c4c/_2022server/config/HandlerInterceptorImpl.java
@@ -1,8 +1,13 @@
 package com.c4c._2022server.config;
 
+import java.util.Locale;
+
+import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,20 +16,22 @@ import com.c4c._2022server.utils.JWTUtils;
 
 @Component
 public class HandlerInterceptorImpl implements HandlerInterceptor {
+    @Autowired
+    MessageSource messageSource;
+
     /**
      * コントローラーの実行前の共通処理
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // Cookieに保存されているJWTを取得する
         String jwt = CookieUtils.getCookie(request, "jwt");
         // JWTを検証
         if (JWTUtils.verifyJWT(jwt)) {
-            // true:コントローラーを実行する
+            // コントローラーを実行する
             return true;
         } else {
-            // false:コントローラーの処理を実行せずに200のレスポンスを返す
-            return false;
+            throw new AuthenticationException(messageSource.getMessage("error.authentication", new String[]{}, Locale.getDefault()));
         }
     }
 }
