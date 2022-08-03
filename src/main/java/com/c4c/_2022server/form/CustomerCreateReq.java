61024038e8968e5f7116e0a77330953d9cf01461
k-takase36 <k.takase36@gmail.com>
2022-08-03 18:49:43 +0900

    顧客登録の必須項目以外の必須設定を外す

--- src/main/java/com/c4c/_2022server/form/CustomerCreateReq.java
+++ src/main/java/com/c4c/_2022server/form/CustomerCreateReq.java
@@ -1,9 +1,12 @@
 package com.c4c._2022server.form;
 
+import java.util.Date;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
+
 
 @Data
 public class CustomerCreateReq {
@@ -32,51 +35,43 @@ public class CustomerCreateReq {
      */
     @NotEmpty(message = "{firstNameKana}{NotEmpty}")
     private String firstNameKana;
-//    /**
-//     * 生年月日
-//     */
-//    @NotNull(message = "{birthday}")
-//    private Date birthday;
-//    /**
-//     * 年齢
-//     */
-//    @NotEmpty(message = "{age}")
-//    private String age;
+    /**
+     * 生年月日
+     */
+    private Date birthday;
+    /**
+     * 年齢
+     */
+    private String age;
     /**
      * 性別
      */
     @NotNull(message = "{gender}{NotNull}")
     private String gender;
-//    /**
-//     * 郵便番号
-//     */
-//    @NotEmpty(message = "{postalCode}")
-//    private String postalCode;
-//    /**
-//     * 都道府県
-//     */
-//    @NotEmpty(message = "{prefectureId}")
-//    private String prefectureId;
-//    /**
-//     * 市区町村
-//     */
-//    @NotEmpty(message = "{address1}")
-//    private String address1;
-//    /**
-//     * 市区町村以下
-//     */
-//    @NotEmpty(message = "{address2}")
-//    private String address2;
-//    /**
-//     * 建物、部屋番号
-//     */
-//    @NotEmpty(message = "{address3}")
-//    private String address3;
-//    /**
-//     * 電話番号
-//     */
-//    @NotEmpty(message = "{phoneNumber}")
-//    private String phoneNumber;
+    /**
+     * 郵便番号
+     */
+    private String postalCode;
+    /**
+     * 都道府県
+     */
+    private String prefectureId;
+    /**
+     * 市区町村
+     */
+    private String address1;
+    /**
+     * 市区町村以下
+     */
+    private String address2;
+    /**
+     * 建物、部屋番号
+     */
+    private String address3;
+    /**
+     * 電話番号
+     */
+    private String phoneNumber;
     /**
      * メールアドレス
      */
