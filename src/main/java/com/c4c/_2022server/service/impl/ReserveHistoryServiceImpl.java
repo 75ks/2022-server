12179c8a6f4ff3640c1e829a75f5720d90d6577f
tkykymok <tkykymok2@icloud.com>
2022-08-06 14:53:49 +0900

    リファクタリングを実施

--- src/main/java/com/c4c/_2022server/service/impl/ReserveHistoryServiceImpl.java
+++ src/main/java/com/c4c/_2022server/service/impl/ReserveHistoryServiceImpl.java
@@ -1,18 +1,16 @@
 package com.c4c._2022server.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.c4c._2022server.constants.ReserveState;
 import com.c4c._2022server.entity.ReserveHistory0001;
 import com.c4c._2022server.form.ReserveHistoryReq;
 import com.c4c._2022server.form.ReserveHistoryRes;
 import com.c4c._2022server.mapper.ReserveHistoryMapper;
 import com.c4c._2022server.service.ReserveHistoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReserveHistoryServiceImpl implements ReserveHistoryService {
@@ -25,11 +23,6 @@ public class ReserveHistoryServiceImpl implements ReserveHistoryService {
      */
     @Override
     public List<ReserveHistoryRes> index(int storeId, ReserveHistoryReq reqForm) {
-        // 予約状態に値がある場合、名前からコードを取得し、設定する
-        if (!(reqForm.getReserveState() == null || reqForm.getReserveState().isEmpty() || reqForm.getReserveState().equals("指定なし"))) {
-            String reserveState = ReserveState.getByName(reqForm.getReserveState()).getCode().toString();
-            reqForm.setReserveState(reserveState);
-        }
         // SELECT文を実行し、データを取得する
         List<ReserveHistory0001> reserveHistoryList = reserveHistoryMapper.select0001(storeId, reqForm);
         // Formにデータを詰める
@@ -37,7 +30,7 @@ public class ReserveHistoryServiceImpl implements ReserveHistoryService {
         for (ReserveHistory0001 reserveHistory0001 : reserveHistoryList) {
             ReserveHistoryRes resForm = new ReserveHistoryRes();
             BeanUtils.copyProperties(reserveHistory0001, resForm);
-            resForm.setReserveState(ReserveState.getByCode(reserveHistory0001.getReserveState()).getName());
+            resForm.setReserveStateName(reserveHistory0001.getReserveStateName());
             resFormList.add(resForm);
         }
         return resFormList;
