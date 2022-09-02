7a1bb0a7deb5945e6dce8ca899459b61fdd265c3
tkykymok <tkykymok2@icloud.com>
2022-09-03 08:00:22 +0900

    消されてしまった記述を復元

--- src/main/java/com/c4c/_2022server/controller/SelectOptionController.java
+++ src/main/java/com/c4c/_2022server/controller/SelectOptionController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.security.sasl.AuthenticationException;
 
+import com.c4c._2022server.entity.*;
+import com.c4c._2022server.mapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +14,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.c4c._2022server.entity.MenuHeader;
-import com.c4c._2022server.entity.MenuHeaderExample;
-import com.c4c._2022server.entity.RankByStore;
-import com.c4c._2022server.entity.RankByStoreExample;
 import com.c4c._2022server.enums.GenderEnum;
 import com.c4c._2022server.enums.PrefectureIdEnum;
 import com.c4c._2022server.enums.ReserveStateEnum;
 import com.c4c._2022server.form.SelectOption;
-import com.c4c._2022server.mapper.MenuHeaderMapper;
-import com.c4c._2022server.mapper.RankByStoreMapper;
-import com.c4c._2022server.mapper.StoreHeaderMapper;
 import com.c4c._2022server.utils.JWTUtils;
 
 @RestController
@@ -34,6 +29,10 @@ public class SelectOptionController {
     MenuHeaderMapper menuHeaderMapper;
     @Autowired
     StoreHeaderMapper storeHeaderMapper;
+    @Autowired
+    CustomerMapper customerMapper;
+    @Autowired
+    StuffMapper stuffMapper;
 
     @GetMapping("/ranks")
     public ResponseEntity<List<SelectOption>> getRanksOptions(@RequestHeader("Authorization") String jwt) throws AuthenticationException {
@@ -129,12 +128,12 @@ public class SelectOptionController {
         }
         return ResponseEntity.ok(selectOptionList);
     }
-    
+
     @GetMapping("/prefectureId")
     public ResponseEntity<List<SelectOption>> getPrefectureIdOptions() {
     	// 都道府県Enum取得
     	PrefectureIdEnum[] enumArray = PrefectureIdEnum.values();
-    	
+
     	// 選択肢一覧を格納するリストをnewする
     	List<SelectOption> selectOptionList = new ArrayList<>();
     	// 検索結果全件に対しての処理
@@ -148,5 +147,54 @@ public class SelectOptionController {
     	}
     	return ResponseEntity.ok(selectOptionList);
     }
-  
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<SelectOption>> getCustomersOptions(@RequestHeader("Authorization") String jwt) throws AuthenticationException {
+        // JWTから店舗IDを取得する
+        JWTUtils instance = JWTUtils.getInstance();
+        Integer storeId = instance.getStoreId(jwt);
+
+        // 店舗IDに紐づく顧客一覧を取得する
+        CustomerExample customerExample = new CustomerExample();
+        customerExample.createCriteria().andStoreIdEqualTo(storeId);
+        List<Customer> customerList = customerMapper.selectByExample(customerExample);
+
+        // 選択肢一覧を格納するリストをnewする
+        List<SelectOption> selectOptionList = new ArrayList<>();
+        // 検索結果全件に対しての処理
+        for (Customer customer : customerList) {
+            SelectOption tempSelectOption = new SelectOption();
+            // selectOptionFormに以下の値を設定
+            tempSelectOption.setCode(customer.getCustomerId()); // 顧客ID
+            tempSelectOption.setName(customer.getLastName() + " " + customer.getFirstName()); // 顧客名
+            // selectOptionListに追加
+            selectOptionList.add(tempSelectOption);
+        }
+        return ResponseEntity.ok(selectOptionList);
+    }
+
+    @GetMapping("/stuffs")
+    public ResponseEntity<List<SelectOption>> getStuffsOptions(@RequestHeader("Authorization") String jwt) throws AuthenticationException {
+        // JWTから店舗IDを取得する
+        JWTUtils instance = JWTUtils.getInstance();
+        Integer storeId = instance.getStoreId(jwt);
+
+        // 店舗IDに紐づくスタッフ一覧を取得する
+        StuffExample stuffExample = new StuffExample();
+        stuffExample.createCriteria().andStoreIdEqualTo(storeId);
+        List<Stuff> stuffList = stuffMapper.selectByExample(stuffExample);
+
+        // 選択肢一覧を格納するリストをnewする
+        List<SelectOption> selectOptionList = new ArrayList<>();
+        // 検索結果全件に対しての処理
+        for (Stuff stuff : stuffList) {
+            SelectOption tempSelectOption = new SelectOption();
+            // selectOptionFormに以下の値を設定
+            tempSelectOption.setCode(stuff.getStuffId()); // スタッフID
+            tempSelectOption.setName(stuff.getLastName() + " " + stuff.getFirstName()); // スタッフ名
+            // selectOptionListに追加
+            selectOptionList.add(tempSelectOption);
+        }
+        return ResponseEntity.ok(selectOptionList);
+    }
 }
