05844a342e576492c037a722ae0d56cb3bf7a8c9
75ks <kenta.s705@gmail.com>
2022-09-06 23:41:38 +0900

    顧客登録APIでパスワードを生成し、顧客登録完了メールを送信するように修正した

--- src/main/java/com/c4c/_2022server/service/impl/CustomerCreateServiceImpl.java
+++ src/main/java/com/c4c/_2022server/service/impl/CustomerCreateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.c4c._2022server.service.impl;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,14 @@ import com.c4c._2022server.entity.Customer;
 import com.c4c._2022server.form.CustomerCreateReq;
 import com.c4c._2022server.mapper.CustomerMapper;
 import com.c4c._2022server.service.CustomerCreateService;
+import com.c4c._2022server.utils.MailUtils;
 
 @Service
 public class CustomerCreateServiceImpl implements CustomerCreateService {
     @Autowired
     CustomerMapper customerMapper;
+    @Autowired
+    MailUtils mailUtils;
 
     @Override
     public void register(int storeId, CustomerCreateReq reqForm) {
@@ -33,13 +37,35 @@ public class CustomerCreateServiceImpl implements CustomerCreateService {
         customer.setAddress3(reqForm.getAddress3());
         customer.setPhoneNumber(reqForm.getPhoneNumber());
         customer.setEmail(reqForm.getEmail());
-
-        // TODO null不許可のため仮実装
-        // パスワード生成、メール送信処理を別途追加する
-        customer.setPassword("pass1234");
+        // パスワード生成
+        String randomPassword = createRandomPassword();
+        customer.setPassword(randomPassword);
 
         // INSERTを実行し、データを登録する
         customerMapper.insert(customer);
+
+        // 顧客登録完了メールを送信
+        mailUtils.sendMail(customer);
+    }
+
+    /**
+     * 10桁のランダムなパスワードを生成
+     * @return password パスワード
+     */
+    private String createRandomPassword() {
+        StringBuilder sb = new StringBuilder("abcdefghijklmnopqrstuvwxyz")
+                .append("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                .append("0123456789");
+
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int num = random.nextInt(sb.length());
+            password.append(sb.charAt(num));
+        }
+
+        return password.toString();
     }
 
 }
