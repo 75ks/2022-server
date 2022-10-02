827db8ff4626fa92e001eb84260a0830e59fd4ff
kuniokuniyama <sum.kuth@gmail.com>
2022-10-02 18:45:32 +0900

    aaa

---
+++ src/main/java/com/c4c/_2022server/controller/CustomerProfileController.java
@@ -0,0 +1,36 @@
+package com.c4c._2022server.controller;
+
+import java.util.List;
+
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.c4c._2022server.form.StuffListFormReq;
+import com.c4c._2022server.form.StuffListFormRes;
+import com.c4c._2022server.service.impl.StuffListServiceImpl;
+import com.c4c._2022server.utils.JWTUtils;
+
+@RestController
+@RequestMapping("/stuffs")
+public class CustomerProfileController {
+    @Autowired
+    StuffListServiceImpl stuffListServiceImpl;
+
+    /**
+     * スタッフ一覧取得
+     * @return List{@literal<StuffListRes>}
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<StuffListFormRes>> index(@RequestHeader("Authorization") String jwt, StuffListFormReq reqForm) throws AuthenticationException {
+    	JWTUtils instance = JWTUtils.getInstance();
+    	int storeId = instance.getStoreId(jwt);
+    	List<StuffListFormRes> stuffFormList = stuffListServiceImpl.index(storeId, reqForm);
+        return ResponseEntity.ok(stuffFormList);
+    }
+}
---
+++ src/main/java/com/c4c/_2022server/service/impl/CustomerProfileServiceImpl.java
@@ -0,0 +1,44 @@
+package com.c4c._2022server.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.c4c._2022server.entity.CustomerList0001;
+import com.c4c._2022server.form.CustomerListFormReq;
+import com.c4c._2022server.form.CustomerListFormRes;
+import com.c4c._2022server.mapper.CustomerMapper;
+import com.c4c._2022server.service.CustomerProfileService;
+
+@Service
+public class CustomerProfileServiceImpl implements CustomerProfileService {
+    @Autowired
+    CustomerMapper customerMapper;
+
+    /**
+     * スタッフ一覧取得
+     * @return customerFormList
+     */
+    @Override
+    public List<CustomerListFormRes> index(int customerId, CustomerListFormReq reqForm) {
+        // SELECT文を実行し、データを取得する
+    	List<CustomerList0001> customerList = customerMapper.select0002(customerId, reqForm);
+        // Formにデータを詰める
+        List<CustomerListFormRes> customerFormList = new ArrayList<>();
+        for (CustomerList0001 customerList0001 : customerList) {
+        	CustomerListFormRes customerForm = new CustomerListFormRes();
+        	customerForm.setCustomerId(customerList0001.getCustomerId());
+        	customerForm.setLastName(customerList0001.getLastName());
+        	customerForm.setFirstName(customerList0001.getFirstName());
+        	customerForm.setLastNameKana(customerList0001.getLastNameKana());
+        	customerForm.setFirstNameKana(customerList0001.getFirstNameKana());
+        	customerForm.setBirthday(customerList0001.getBirthday());
+        	customerForm.setAge(customerList0001.getAge());
+        	customerForm.setGender(customerList0001.getGender());
+        	customerFormList.add(customerForm);
+        }
+        return customerFormList;
+    }
+}
