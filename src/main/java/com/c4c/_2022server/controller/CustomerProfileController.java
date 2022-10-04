2ff8aa6d1823fd9d87166590172c761718b68d52
kuniokuniyama <sum.kuth@gmail.com>
2022-10-04 22:02:32 +0900

    aaa

--- src/main/java/com/c4c/_2022server/controller/CustomerProfileController.java
+++ src/main/java/com/c4c/_2022server/controller/CustomerProfileController.java
@@ -11,26 +11,26 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.c4c._2022server.form.StuffListFormReq;
-import com.c4c._2022server.form.StuffListFormRes;
-import com.c4c._2022server.service.impl.StuffListServiceImpl;
+import com.c4c._2022server.form.CustomerListFormReq;
+import com.c4c._2022server.form.CustomerListFormRes;
+import com.c4c._2022server.service.impl.CustomerProfileServiceImpl;
 import com.c4c._2022server.utils.JWTUtils;
 
 @RestController
-@RequestMapping("/stuffs")
+@RequestMapping("/customer")
 public class CustomerProfileController {
     @Autowired
-    StuffListServiceImpl stuffListServiceImpl;
+    CustomerProfileServiceImpl CustomerProfileServiceImpl;
 
     /**
      * スタッフ一覧取得
      * @return List{@literal<StuffListRes>}
      */
-    @GetMapping("/")
-    public ResponseEntity<List<StuffListFormRes>> index(@RequestHeader("Authorization") String jwt, StuffListFormReq reqForm) throws AuthenticationException {
+    @GetMapping("/profile")
+    public ResponseEntity<List<CustomerListFormRes>> index(@RequestHeader("Authorization") String jwt, CustomerListFormReq reqForm) throws AuthenticationException {
     	JWTUtils instance = JWTUtils.getInstance();
-    	int storeId = instance.getStoreId(jwt);
-    	List<StuffListFormRes> stuffFormList = stuffListServiceImpl.index(storeId, reqForm);
-        return ResponseEntity.ok(stuffFormList);
+    	Integer customerId = instance.getId(jwt);
+    	List<CustomerListFormRes> customerFormList = CustomerProfileServiceImpl.index(customerId, reqForm);
+        return ResponseEntity.ok(customerFormList);
     }
 }
