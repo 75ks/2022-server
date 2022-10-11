27d0879d4891db4bb6e1c3c1ccb66111131e6b52
75ks <kenta.s705@gmail.com>
2022-10-11 23:00:32 +0900

    ランクID取得APIを作成した

---
+++ src/main/java/com/c4c/_2022server/form/RegisterReserveRankIdReq.java
@@ -0,0 +1,11 @@
+package com.c4c._2022server.form;
+
+import lombok.Data;
+
+@Data
+public class RegisterReserveRankIdReq {
+    /**
+     * スタッフID
+     */
+    private Integer stuffId;
+}
--- src/main/java/com/c4c/_2022server/service/ReserveRegisterService.java
+++ src/main/java/com/c4c/_2022server/service/ReserveRegisterService.java
@@ -1,8 +1,15 @@
 package com.c4c._2022server.service;
 
+import com.c4c._2022server.form.RegisterReserveRankIdReq;
 import com.c4c._2022server.form.ReserveRegisterReq;
 
 public interface ReserveRegisterService {
+    /**
+     * ランクID取得
+     * @param reqForm
+     */
+    public Integer getRankId(RegisterReserveRankIdReq reqForm);
+    
     /**
      * 予約情報登録
      * @param customerId
