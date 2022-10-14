4ced4d6e6d73ec3ff3ea0fdabb177ba8d8a88f12
daniel tsukada <daniel.tsucada78@gmail.com>
2022-10-14 13:20:09 +0900

    Formクラスの追加

---
+++ src/main/java/com/c4c/_2022server/form/MenuManagementDetailUpdateReq.java
@@ -0,0 +1,17 @@
+package com.c4c._2022server.form;
+
+import javax.validation.constraints.NotNull;
+
+public class MenuManagementDetailUpdateReq {
+	/**
+	 * ランクID
+	 */
+	@NotNull(message = "{rank}{NotNull}")
+	private Integer rankId;
+	/**
+	 * プライス
+	 */
+	@NotNull(message = "{price}{NotEmpty}")
+	private Integer price;
+
+}
---
+++ src/main/java/com/c4c/_2022server/form/MenuManegementUpdateReq.java
@@ -0,0 +1,14 @@
+package com.c4c._2022server.form;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MenuManegementUpdateReq {
+	
+	private List<MenuManagementMenuIdUpdateReq> MenuId;
+
+
+
+}
