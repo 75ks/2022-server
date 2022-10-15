260f35de9c9b47e86b16380c49b893803f0dcdd9
daniel tsukada <daniel.tsucada78@gmail.com>
2022-10-16 01:44:09 +0900

    formクラス名の修正

--- src/main/java/com/c4c/_2022server/form/MenuManagementDetailUpdateReq.java
+++ src/main/java/com/c4c/_2022server/form/MenuManagementDetailUpdateReq.java
@@ -2,17 +2,20 @@ package com.c4c._2022server.form;
 
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
 public class MenuManagementDetailUpdateReq {
 	/**
-	 * ランクID
+	 * プライス
 	 */
-	@NotNull(message = "{rank}{NotNull}")
+	@NotNull(message = "{rankId}{NotEmpty}")
 	private Integer rankId;
+
 	/**
 	 * プライス
 	 */
 	@NotNull(message = "{price}{NotEmpty}")
 	private Integer price;
-	
-	
+
 }
---
+++ src/main/java/com/c4c/_2022server/form/MenuManegementUnityUpdateReq.java
@@ -0,0 +1,12 @@
+package com.c4c._2022server.form;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MenuManegementUnityUpdateReq {
+	
+	private List<MenuManagementUpdateReq> unity;
+
+}
