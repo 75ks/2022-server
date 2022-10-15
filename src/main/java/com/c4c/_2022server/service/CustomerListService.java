b3e170287f16b5dc461a4a6a9e537d4ccb6f3abd
k-takase36 <k.takase36@gmail.com>
2022-10-15 23:42:21 +0900

    別画面の作業でおそらくコメントアウトされていたため、コメントアウトを外した

--- src/main/java/com/c4c/_2022server/service/CustomerListService.java
+++ src/main/java/com/c4c/_2022server/service/CustomerListService.java
@@ -7,7 +7,7 @@ import com.c4c._2022server.form.CustomerListFormRes;
 
 public interface CustomerListService {
     /**
-     * スタッフ一覧取得
+     * 顧客一覧取得
      */
     List<CustomerListFormRes> index(int customerId, CustomerListFormReq reqForm);
 }
