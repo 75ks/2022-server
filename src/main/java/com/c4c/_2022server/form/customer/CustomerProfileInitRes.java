981723d5e0bef4f976bd3c126f0ce09dd70476d4
tkykymok <tkykymok2@icloud.com>
2022-10-15 19:23:41 +0900

    CustomerProfile初期表示イベント用のレスポンスFormを別で作成

---
+++ src/main/java/com/c4c/_2022server/form/customer/CustomerProfileInitRes.java
@@ -0,0 +1,180 @@
+package com.c4c._2022server.form.customer;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CustomerProfileInitRes {
+	/**
+	 * Table: CUSTOMER
+     * Column: CUSTOMER_ID
+     * Type: INT
+     * Remark: 顧客ID
+     */
+    private Integer customerId;
+
+    /**
+     * Table: CUSTOMER
+     * Column: LAST_NAME
+     * Type: VARCHAR(30)
+     * Remark: 姓
+     */
+    private String lastName;
+
+    /**
+     * Table: CUSTOMER
+     * Column: FIRST_NAME
+     * Type: VARCHAR(30)
+     * Remark: 名
+     */
+    private String firstName;
+
+    /**
+     * Table: CUSTOMER
+     * Column: LAST_NAME_KANA
+     * Type: VARCHAR(30)
+     * Remark: セイ
+     */
+    private String lastNameKana;
+
+    /**
+     * Table: CUSTOMER
+     * Column: FIRST_NAME_KANA
+     * Type: VARCHAR(30)
+     * Remark: メイ
+     */
+    private String firstNameKana;
+
+    /**
+     * Table: CUSTOMER
+     * Column: BIRTHDAY
+     * Type: DATE
+     * Remark: 生年月日
+     */
+    private String birthday;
+
+    /**
+     * Table: CUSTOMER
+     * Column: AGE
+     * Type: INT
+     * Remark: 年齢
+     */
+    private Integer age;
+
+    /**
+     * Table: CUSTOMER
+     * Column: GENDER
+     * Type: INT
+     * Remark: 性別
+     */
+    private Integer gender;
+
+    /**
+     * Column: POSTAL_CODE
+     * Type: VARCHAR(7)
+     * Remark: 郵便番号
+     */
+    private String postalCode;
+
+    /**
+     * Column: PREFECTURE_ID
+     * Type: INT
+     * Remark: 都道府県
+     */
+    private Integer prefectureId;
+
+    /**
+     * Column: ADDRESS1
+     * Type: VARCHAR(45)
+     * Remark: 住所1
+     */
+    private String address1;
+
+    /**
+     * Column: ADDRESS2
+     * Type: VARCHAR(45)
+     * Remark: 住所2
+     */
+    private String address2;
+
+    /**
+     * Column: ADDRESS3
+     * Type: VARCHAR(45)
+     * Remark: 住所3
+     */
+    private String address3;
+
+    /**
+     * Column: PHONE_NUMBER
+     * Type: VARCHAR(11)
+     * Remark: 電話番号
+     */
+    private String phoneNumber;
+
+    /**
+     * Column: EMAIL
+     * Type: VARCHAR(255)
+     * Remark: メールアドレス
+     */
+    private String email;
+
+    /**
+     * Column: PASSWORD
+     * Type: VARCHAR(255)
+     * Remark: パスワード
+     */
+    private String password;
+
+    /**
+     * Column: FIRST_LOGIN_FLG
+     * Type: INT
+     * Default value: 0
+     * Remark: 初回ログインフラグ
+     */
+    private Integer firstLoginFlg;
+
+    /**
+     * Column: DELETE_FLG
+     * Type: INT
+     * Default value: 0
+     * Remark: 削除フラグ
+     */
+    private Integer deleteFlg;
+
+    /**
+     * Column: CREATED_DATETIME
+     * Type: TIMESTAMP
+     * Remark: 作成日時
+     */
+    private LocalDateTime createdDatetime;
+
+    /**
+     * Column: CREATED_USER
+     * Type: INT
+     * Remark: 作成者ID
+     */
+    private Integer createdUser;
+
+    /**
+     * Column: UPDATE_DATETIME
+     * Type: TIMESTAMP
+     * Remark: 更新日時
+     */
+    private LocalDateTime updateDatetime;
+
+    /**
+     * Column: UPDATE_USER
+     * Type: INT
+     * Remark: 更新者ID
+     */
+    private Integer updateUser;
+
+    /**
+     * Column: VERSION_EX_KEY
+     * Type: INT
+     * Default value: 0
+     * Remark: 排他制御カラム
+     */
+    private Integer versionExKey;
+}
