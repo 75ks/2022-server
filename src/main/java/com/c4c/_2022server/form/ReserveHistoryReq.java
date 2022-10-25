430059f980f0a467ba47950a52a1afd94e072383
75ks <kenta.s705@gmail.com>
2022-10-25 20:15:18 +0900

    予約履歴一覧機能コメント修正

--- src/main/java/com/c4c/_2022server/form/ReserveHistoryReq.java
+++ src/main/java/com/c4c/_2022server/form/ReserveHistoryReq.java
@@ -4,33 +4,53 @@ import lombok.Data;
 
 @Data
 public class ReserveHistoryReq {
-    /** 予約履歴ID */
+    /**
+     * 予約履歴ID
+     */
     private String reserveHistoryId;
 
-    /** 顧客名 */
+    /**
+     * 顧客名
+     */
     private String customerName;
 
-    /** スタッフ名 */
+    /**
+     * スタッフ名
+     */
     private String stuffName;
 
-    /** ランク名 */
+    /**
+     * ランク名
+     */
     private String rankName;
 
-    /** メニュー */
+    /**
+     * メニュー
+     */
     private String menu;
 
-    /** 料金(下限) */
+    /**
+     * 料金(下限)
+     */
     private Integer priceMin;
 
-    /** 料金(上限) */
+    /**
+     * 料金(上限)
+     */
     private Integer priceMax;
 
-    /** 日時(下限) */
+    /**
+     * 日時(下限)
+     */
     private String reserveDateTimeMin;
 
-    /** 日時(上限) */
+    /**
+     * 日時(上限)
+     */
     private String reserveDateTimeMax;
 
-    /** 予約状態 */
+    /**
+     * 予約状態
+     */
     private Integer reserveState;
 }
