fcd8dc10259201f44bf984533b447eb6378d33c6
75ks <kenta.s705@gmail.com>
2022-10-25 20:34:04 +0900

    売上集計機能コメント修正

--- src/main/java/com/c4c/_2022server/service/SalesTotalService.java
+++ src/main/java/com/c4c/_2022server/service/SalesTotalService.java
@@ -11,36 +11,41 @@ import com.c4c._2022server.form.SalesTotalYearRes;
 public interface SalesTotalService {
     /**
      * 売上情報取得(年)
-     * @param storeId
-     * @param salesYear
+     * @param storeId 店舗ID
+     * @param salesYear 年(YYYY)
+     * @return パラメータ年の一年分の売上データ
      */
     public List<SalesTotalYearRes> getSalesYear(int storeId, String salesYear);
 
     /**
      * 売上情報取得(月)
-     * @param storeId
-     * @param salesYearMonth
+     * @param storeId 店舗ID
+     * @param salesYearMonth 月(M)
+     * @return パラメータ月の売上データ
      */
     public SalesTotalMonthRes getSalesMonth(int storeId, String salesYearMonth);
 
     /**
      * 売上情報取得(グラフ)
-     * @param storeId
-     * @param salesYear
+     * @param storeId 店舗ID
+     * @param salesYear 年(YYYY)
+     * @return パラメータ年の一年分の売上データ(グラフ)
      */
     public List<SalesTotalChartRes> getSalesChart(int storeId, String salesYear);
 
     /**
-     * 売上情報取得(円グラフ)
-     * @param storeId
-     * @param salesYearMonth
+     * 売上情報取得(グラフ)
+     * @param storeId 店舗ID
+     * @param salesYearMonth 月(M)
+     * @return パラメータ月の売上データ(グラフ)
      */
     public List<SalesTotalPieChartRes> getSalesPieChart(int storeId, String salesYearMonth);
 
     /**
      * 売上情報一覧取得
-     * @param storeId
-     * @param salesYearMonth
+     * @param storeId 店舗ID
+     * @param salesYearMonth 月(M)
+     * @return 売上情報一覧
      */
     public List<SalesHistoryRes> index(int storeId, String salesYearMonth);
 }
