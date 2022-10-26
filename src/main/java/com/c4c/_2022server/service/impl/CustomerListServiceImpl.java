ffd9aed0766a7096519e7ab7d0340128f196fbef
kuniokuniyama <sum.kuth@gmail.com>
2022-10-26 22:48:17 +0900

    エラー起きてしまったので修正

--- src/main/java/com/c4c/_2022server/service/impl/CustomerListServiceImpl.java
+++ src/main/java/com/c4c/_2022server/service/impl/CustomerListServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.c4c._2022server.entity.CustomerList0001;
+import com.c4c._2022server.entity.Customer0002;
 import com.c4c._2022server.form.CustomerListFormReq;
 import com.c4c._2022server.form.CustomerListFormRes;
 import com.c4c._2022server.mapper.CustomerMapper;
@@ -24,17 +24,16 @@ public class CustomerListServiceImpl implements CustomerListService {
     @Override
     public List<CustomerListFormRes> index(int storeId, CustomerListFormReq reqForm) {
         // SELECT文を実行し、データを取得する
-        List<CustomerList0001> customerList = customerMapper.select0002(storeId, reqForm);
+        List<Customer0002> customerList = customerMapper.select0002(storeId, reqForm);
         // Formにデータを詰める
         List<CustomerListFormRes> customerFormList = new ArrayList<>();
-        for (CustomerList0001 customerList0001 : customerList) {
+        for (Customer0002 customerList0001 : customerList) {
         	CustomerListFormRes customerForm = new CustomerListFormRes();
         	customerForm.setCustomerId(customerList0001.getCustomerId());
         	customerForm.setLastName(customerList0001.getLastName());
         	customerForm.setFirstName(customerList0001.getFirstName());
         	customerForm.setLastNameKana(customerList0001.getLastNameKana());
         	customerForm.setFirstNameKana(customerList0001.getFirstNameKana());
-        	customerForm.setBirthday(customerList0001.getBirthday());
         	customerForm.setAge(customerList0001.getAge());
         	customerForm.setGender(customerList0001.getGender());
         	customerFormList.add(customerForm);
