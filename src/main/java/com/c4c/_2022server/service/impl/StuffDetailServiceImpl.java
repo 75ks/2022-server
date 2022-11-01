defb70b1a899e8acbc49e389ac24d47acb184006
75ks <kenta.s705@gmail.com>
2022-11-01 23:57:44 +0900

    スタッフ情報更新にメールアドレス登録済みチェックを追加した

--- src/main/java/com/c4c/_2022server/service/impl/StuffDetailServiceImpl.java
+++ src/main/java/com/c4c/_2022server/service/impl/StuffDetailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.c4c._2022server.service.impl;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import com.c4c._2022server.entity.Stuff;
@@ -21,6 +23,8 @@ public class StuffDetailServiceImpl implements StuffDetailService {
     StuffMapper stuffMapper;
     @Autowired
     EntityUtils entityUtils;
+    @Autowired
+    MessageSource messageSource;
 
     /**
      * 初期表示
@@ -50,8 +54,21 @@ public class StuffDetailServiceImpl implements StuffDetailService {
      */
     @Override
     public void register(int stuffId, int storeId, StuffDetailRegisterReq reqForm) throws DuplicationException {
+        // 更新対象のスタッフ情報を取得
+        StuffExample stuffExample = new StuffExample();
+        stuffExample.createCriteria()
+                .andStuffIdEqualTo(reqForm.getStuffId()); // スタッフID
+        Stuff stuff = stuffMapper.selectByExample(stuffExample) // 検索を行う
+                .stream() // streamに変換する
+                .findFirst() // 先頭の1件を取得する
+                .orElse(null); // 先頭の1件が取得できない場合は、nullを返す
+
+        // メールアドレスが登録済み かつ 別スタッフのメールアドレスかチェック
+        Stuff checkStuff = stuffMapper.select0001(reqForm.getEmail());
+        if (checkStuff != null && !(checkStuff.getEmail().equals(stuff.getEmail()))) {
+            throw new DuplicationException(messageSource.getMessage("error.email.registered", new String[]{}, Locale.getDefault()));
+        }
         // Formにデータを詰める
-        Stuff stuff = new Stuff();
         BeanUtils.copyProperties(reqForm, stuff);
         // UPDATE時の共通設定
         entityUtils.setColumns4Update(stuff, stuffId);
